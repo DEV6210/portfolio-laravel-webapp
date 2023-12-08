9793847fa7767fae0b5b045b5c05c6b3db771dcb
Amit Mandal <91625966+Developer-AmitMandal@users.noreply.github.com>
2023-12-08 17:01:25 +0530

    updated

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
  <!-- Footer -->
  <footer>
      <span>
-         <a href="https://www.linkedin.com/in/akashmahapatra"> Akash Mahapatra</a> | <span
+         <a href="https://www.linkedin.com/in/akashmahapatra"> Amit Mandal</a> | <span
              class="far fa-copyright"></span> 2022 All Rights Reserved. Privacy Policy
      </span>
  </footer>
